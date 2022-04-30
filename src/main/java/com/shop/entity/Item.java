d2ed64829fab13b6525f7d632753432cbe9ffe7e
Juna <jn307742@gmail.com>
2022-04-30 14:41:04 +0900

    Order Cancel

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -57,4 +57,8 @@ public class Item extends BaseEntity {
         }
         this.stockNumber = restStock;
     }
+
+    public void addStock(int stockNumber) {
+        this.stockNumber += stockNumber;
+    }
 }
