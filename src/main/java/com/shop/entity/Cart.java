cc9b85f089ed26b223587e1f1e3695287bc234e6
Juna <jn307742@gmail.com>
2022-04-30 15:11:27 +0900

    Cart

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -20,4 +20,10 @@ public class Cart extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY) // 일대일 매핑
     @JoinColumn(name = "member_id") // 매핑할 외래키 지정
     private Member member;
+
+    public static Cart createCart(Member member) {
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
