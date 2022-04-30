b69897c5025295972cbd714517dddf59fda556d3
Juna <jn307742@gmail.com>
2022-04-30 17:36:21 +0900

    Cart Order

--- src/main/java/com/shop/dto/CartItemDto.java
+++ src/main/java/com/shop/dto/CartItemDto.java
@@ -12,6 +12,6 @@ public class CartItemDto {
     @NotNull(message = "상품 아이디는 필수 입력 값입니다.")
     private Long itemId;
 
-    @Min(value = 1, message = "최소 1개 이상 답아주세요.")
+    @Min(value = 1, message = "최소 1개 이상 담아주세요.")
     private int count;
 }
