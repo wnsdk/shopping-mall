c1a13227e2029cd2ed09e8a7378669b2eb0448bc
Juna <jn307742@gmail.com>
2022-04-30 13:23:12 +0900

    Order History

---
+++ src/main/java/com/shop/dto/OrderItemDto.java
@@ -0,0 +1,24 @@
+package com.shop.dto;
+
+import com.shop.entity.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderItemDto {
+
+    private String itemNm;
+
+    private int count;
+
+    private int orderPrice;
+
+    private String imgUrl;
+
+    public OrderItemDto(OrderItem orderItem, String imgUrl) {
+        this.itemNm = itemNm;
+        this.count = count;
+        this.orderPrice = orderPrice;
+        this.imgUrl = imgUrl;
+    }
+}
